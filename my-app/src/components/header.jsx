7b39b13b1d9dd24f742ac42a3f00e0a2a3519aa1
Mathilde Plancq <mathilde.plancq@gmail.com>
2023-08-07 16:20:22 +0200

    Responsive correction

--- my-app/src/components/header.jsx
+++ my-app/src/components/header.jsx
@@ -6,8 +6,8 @@ function Header() {
     <div className='header'>
       <img src="/images/LOGO.png" alt="Logo de Kasa" className='header--logo'/>
       <nav className='header--nav'>
-        <NavLink to="/" id='header--link__1' className={({ isActive, isNotActive}) => isNotActive ? "" : isActive ? "active": ""}>Accueil</NavLink>
-        <NavLink to="/A-Propos" id='header--link__2' className={({ isActive, isNotActive}) => isNotActive ? "" : isActive ? "active": ""}>A Propos</NavLink>
+        <NavLink to="/" id='header--link__1' className={({isActive}) => isActive ? "active": ""}>Accueil</NavLink>
+        <NavLink to="/A-Propos" id='header--link__2' className={({isActive}) =>  isActive ? "active": ""}>A Propos</NavLink>
       </nav>
     </div>
   );
